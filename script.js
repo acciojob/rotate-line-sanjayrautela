ef4bffaa681506dc842444e4128d7c0d2da0baa7
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-26 19:37:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const line = document.getElementById("line");
+  let angle = 0;
+
+  function rotateLine() {
+    angle += 2;
+    line.style.transform = `rotate(${angle}deg)`;
+    requestAnimationFrame(rotateLine);
+  }
+
+  rotateLine();
+});
+
